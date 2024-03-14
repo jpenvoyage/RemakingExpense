5a2f9f159862f20da80d4908f6758b575ad55be2
joaobug <joaoenfrance@gmail.com>
2024-03-14 10:29:42 -0300

    Initial Commit

---
+++ RemakingExpense/RemakingExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemakingExpenseApp.swift
+//  RemakingExpense
+//
+//  Created by Joao Leal on 3/14/24.
+//
+
+import SwiftUI
+
+@main
+struct RemakingExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
